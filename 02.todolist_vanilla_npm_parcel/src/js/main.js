9481fb52df548c30ae6f6591740c928c05b7033f
uhjee <oper1490@gmail.com>
2021-05-10 23:57:02 +0900

    [02.todolist_vanilla_npm_parcel]  selectColorBox 선택 완료, (components, datas) module closure 처리, (todoList) module 이름 _handlers로 변경, 타 module들 이름에 언더스코어 추가

--- 02.todolist_vanilla_npm_parcel/src/js/main.js
+++ 02.todolist_vanilla_npm_parcel/src/js/main.js
@@ -1,11 +1,52 @@
 'use script';
 
+// TODO@heojeehaeng - data & UI 를 분리하자.....
+// ! Datas
+// IIFE, Closure - 비공개 변수를 만들어 접근 못하도록
+const _data = (() => {
+  let todoList = [
+    // example: 아래 같은 형식으로 list
+    {
+      color: 'orange',
+      text: '컨텍스트 내용',
+    },
+  ];
+  let todoText = '';
+  let selectedMarkColor = 'green'; // default
+
+  // todoText Setter, Getter
+  const setTodoText = (text) => {
+    todoText = text;
+  };
+  const getTodoText = () => todoText;
+
+  // selectedMarkColor Setter, Getter
+  const setSelectedMarkColor = (color) => {
+    if (!color) return;
+    selectedMarkColor = color;
+  };
+  const getSelectedMarkColor = () => selectedMarkColor;
+
+  // Exposed functions  => Closure
+  return {
+    setSelectedMarkColor: (color) => {
+      setSelectedMarkColor(color);
+    },
+    getSelectedMarkColor: () => getSelectedMarkColor(),
+    setTodoText: (text) => {
+      setTodoText(text);
+    },
+    getTodoText: () => getTodoText(),
+  };
+})();
+
 /**
  *  TODO@heojeehaeng : class로 변경, factory pattern?으로 변경
  * ! Components fragmentDocument와  innerHtml 를 이용해 컴포넌트를 만드는 함수를 반환한다.
  * @return  {[type]}  make[componentName] 네이밍을 가진 컴포넌트 만드는 함수를
  */
-const components = (() => {
+// IIFE, Closure - 비공개 변수를 만들어 접근 못하도록
+const _components = (() => {
   /**
    * todo-item component 를 반환한다.
    *
@@ -14,7 +55,7 @@ const components = (() => {
    * @author  uhjee
    * @return  {[type]}             todo-item component
    */
-  function makeTodoItem(text, markColor = 'orange') {
+  const makeTodoItem = (text) => {
     const docFrag = document.createDocumentFragment();
     // 그릇, 매개체 역할만 하는 빈 div element
     const divEl = document.createElement('div');
@@ -22,19 +63,19 @@ const components = (() => {
     docFrag.appendChild(divEl);
     docFrag.querySelector('div').innerHTML = `
       <li class="todo--body__item">
-          <div class="mark mark--${markColor}"></div>
+          <div class="mark mark--${_data.getSelectedMarkColor()}"></div>
           <div class="content">${text}</div>
       </li>`;
     // why cloneNode? Fragment element는 append 되면 메모리에서 사라짐.
     return docFrag.cloneNode(true).querySelector('div').firstElementChild;
-  }
+  };
 
   /**
    * btn-box component (edit, delete btn) 를 반환한다.
    * @author  uhjee
    * @return  {[type]}  btn-box component (edit, delete btn)
    */
-  function makeBtnBoxForTodoItem() {
+  const makeBtnBoxForTodoItem = () => {
     const docFrag = document.createDocumentFragment();
     const divEl = document.createElement('div');
     docFrag.appendChild(divEl);
@@ -49,25 +90,29 @@ const components = (() => {
       </div>`;
 
     return docFrag.cloneNode(true).querySelector('div').firstElementChild;
-  }
+  };
+
+  /**
+   * mark component를 반환한다.
+   *
+   * @author  uhjee
+   * @param   {[type]}  color  mark의 색
+   *
+   * @return  {[type]}         mark compoenent
+   */
+  const makeMark = () => `
+    <div class="mark mark--${_data.getSelectedMarkColor()}"></div>
+    `;
 
   return {
-    makeTodoItem,
-    makeBtnBoxForTodoItem,
+    makeTodoItem: (text) => makeTodoItem(text),
+    makeBtnBoxForTodoItem: () => makeBtnBoxForTodoItem(),
+    makeMark: () => makeMark(),
   };
 })();
 
-// TODO@heojeehaeng - data & UI 를 분리하자.....
-const data = function () {
-  const todoList = [];
-  return {
-    getTodoListData,
-    addTodoItemData,
-  };
-};
-
 // ! 로직 관련 (이벤트 핸들러)----------------------------------------------------------
-const todoList = {
+const _handler = {
   /**
    * todo--body에 todo-item을 추가한다.
    *
@@ -83,11 +128,8 @@ const todoList = {
       return;
     }
 
-    // TODO@heojeehaeng 색 동적으로 받아오기(green, grey, lightgreen, orange)
-    const markColor = 'green';
-
     const todoBodyEl = document.querySelector('.todo--body');
-    todoBodyEl.appendChild(components.makeTodoItem(inputEl.value, markColor));
+    todoBodyEl.appendChild(_components.makeTodoItem(inputEl.value));
 
     //  input 초기화
     inputEl.value = '';
@@ -108,7 +150,31 @@ const todoList = {
       }
     });
     itemEl.classList.add('focused'); // flag 역할
-    itemEl.appendChild(components.makeBtnBoxForTodoItem());
+    itemEl.appendChild(_components.makeBtnBoxForTodoItem());
+  },
+
+  selectColorMark: (e) => {
+    // TODO@heojeehaeng ::isEmpty로 변경
+    if (![...e.target.classList].includes('selectable')) return;
+    const selectableBtn = e.target;
+    // close 버튼
+    if (selectableBtn.dataset.action === 'close') {
+      selectColorBox.classList.remove('show');
+      return;
+    }
+
+    _data.setSelectedMarkColor(selectableBtn.dataset.color);
+
+    selectColorBtnEl.innerHTML = _components.makeMark();
+    // TODO@uhjee :: DATA BINDING!!!!
+    selectColorBox.classList.remove('show');
+    return;
+  },
+  addClass: (element, className) => () => {
+    element.classList.add(className);
+  },
+  removeClass: (element, className) => () => {
+    element.classList.remove(className);
   },
   delAllEle: function () {
     var list = document.getElementById('listBody');
@@ -196,33 +262,6 @@ const todoList = {
         // checkbox의 부모의 부모 == tr El를 삭제해준다.
         body.removeChild(chkbox[i].parentNode.parentNode);
       }
-    }
-  },
-  /**
-   * 요일(num)에 따른 약어 요일(String)을 반환한다.
-   * @author  uhjee
-   * @param   {[type]}  day  숫자형 day(Date.getDay() 반환값)
-   *
-   * @return  {[type]}       약어 요일(String) e.g. Sun
-   */
-  getDayString: (day) => {
-    switch (day) {
-      case 0:
-        return 'Sun';
-      case 1:
-        return 'Mon';
-      case 2:
-        return 'Tue';
-      case 3:
-        return 'Wen';
-      case 4:
-        return 'Thu';
-      case 5:
-        return 'Fri';
-      case 6:
-        return 'Sat';
-      default:
-        throw new Error('Not exist the day!');
     }
   },
 };
@@ -317,15 +356,57 @@ var doneList = {
     }
   },
 };
+// ! Utility Functions
+const _util = {
+  /**
+   * 요일(num)에 따른 약어 요일(String)을 반환한다.
+   * @author  uhjee
+   * @param   {[type]}  day  숫자형 day(Date.getDay() 반환값)
+   *
+   * @return  {[type]}       약어 요일(String) e.g. Sun
+   */
+  getDayString: (day) => {
+    switch (day) {
+      case 0:
+        return 'Sun';
+      case 1:
+        return 'Mon';
+      case 2:
+        return 'Tue';
+      case 3:
+        return 'Wen';
+      case 4:
+        return 'Thu';
+      case 5:
+        return 'Fri';
+      case 6:
+        return 'Sat';
+      default:
+        throw new Error('Not exist the day!');
+    }
+  },
+};
+
 // 추가 버튼 이벤트 등록
-document.querySelector('#add-btn').addEventListener('click', todoList.addTodo);
-//  document.querySelector("input.text-basic").addEventListener('keyup', todoList.addList);
+// TODO@uhjee :: 추가 DATA BINDING!!!!
+
+// TODO@uhjee :: input-todo validation check
+// document.querySelector('#input-todo').addEventListener('keyup', (e) => {
+//   _data.setTodoText(e.target.value);
+
+//   console.log(_data.getTodoText());
+// });
+
+document.querySelector('#add-btn').addEventListener('click', _handler.addTodo);
+
+document.querySelector('.todo--body__item').addEventListener('');
+//  document.querySelector("input.text-basic").addEventListener('keyup', _handler.addList);
 // // 모두 완료 버튼 이벤트 등록
-// document.getElementById('btnDelAll').addEventListener('click', todoList.delAllEle);
+// document.getElementById('btnDelAll').addEventListener('click', _handler.delAllEle);
 // // 마지막 엘레먼트 완료 버튼 이벤트 등록
-// document.getElementById('btnDelLast').addEventListener('click', todoList.delLastEle);
+// document.getElementById('btnDelLast').addEventListener('click', _handler.delLastEle);
 // // 선택 완료 버튼 이벤트 등록
-// document.getElementById('DeleteSel').addEventListener('click', todoList.delSelected);
+// document.getElementById('DeleteSel').addEventListener('click', _handler.delSelected);
 
 // // 모두 복원 버튼 이벤트 등록
 // document.getElementById('done-btnDelAll').addEventListener('click', doneList.delAllEle);
@@ -358,25 +439,21 @@ searchInputEl.addEventListener('blur', () => {
 //  ! TODO CARD 관련 이벤트.
 // * input-todo 관련
 const inputTodoEl = document.querySelector('#input-todo');
-inputTodoEl.addEventListener('focus', () => {
-  inputTodoEl.classList.add('focused');
-});
-inputTodoEl.addEventListener('blur', () => {
-  inputTodoEl.classList.remove('focused');
-});
+inputTodoEl.addEventListener('focus', _handler.addClass(inputTodoEl, 'focused'));
+inputTodoEl.addEventListener('blur', _handler.removeClass(inputTodoEl, 'focused'));
 
 // 날짜 표현(오늘)
 const thisYearEl = document.querySelector('.this-year');
 const today = new Date();
 
-thisYearEl.textContent = `${today.getFullYear()}. ${today.getMonth() + 1}. ${today.getDate()} (${todoList.getDayString(
+thisYearEl.textContent = `${today.getFullYear()}. ${today.getMonth() + 1}. ${today.getDate()} (${_util.getDayString(
   today.getDay(),
 )})`;
 
 // * todo--item: mouseover, button group -- Event delegattion
 // TODO@heojeehaeng throttle 하게 적용해도 될 듯.. 알아보자
 const todoBodyEl = document.querySelector('.todo--body');
-todoBodyEl.addEventListener('mouseover', todoList.showItemBtnBox);
+todoBodyEl.addEventListener('mouseover', _handler.showItemBtnBox);
 
 // * select-color-box 관련
 const selectColorBtnEl = document.querySelector('.select-color-btn');
@@ -385,15 +462,4 @@ selectColorBtnEl.addEventListener('click', () => {
   selectColorBox.classList.add('show');
 });
 
-selectColorBox.addEventListener('click', (e) => {
-  // if(e.target.dataset
-  // TODO@heojeehaeng ::isEmpty로 변경
-  if (![...e.target.classList].includes('selectable')) return;
-  console.log(e.target.dataset.action);
-  if (e.target.dataset.action === 'close') {
-    selectColorBox.classList.remove('show');
-    return;
-  }
-
-  // console.log(!!e.target.dataset);
-});
+selectColorBox.addEventListener('click', _handler.selectColorMark);
